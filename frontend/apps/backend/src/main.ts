cc0e17167313dfc70f7c459de862a9be4c13b837
van-goghinho <ili.an.bon.sens@protonmail.com>
2026-03-11 12:31:04 +0100

    feat: backend dockerisé, prefix /api, CORS prod

--- frontend/apps/backend/src/main.ts
+++ frontend/apps/backend/src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // Frontend URLs
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://217.182.204.220:3000',  // Frontend prod
+    ],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3001}`);
 }
 bootstrap();
